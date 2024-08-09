d8f6b6afc6eceb6da5fc40032b7015a31751b9e2
yxhej <wui@bxscience.edu>
2024-08-09 18:19:13 -0400

    clownery

--- sketch.ino
+++ sketch.ino
@@ -13,6 +13,7 @@
 // Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
 // Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, , -1);
 
+
 // DHT22 declaration
 DHT dht(DHTPIN, DHTTYPE);
 
