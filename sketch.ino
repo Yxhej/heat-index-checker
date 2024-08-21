302a8f8b5b9ed898f4796cb8fea6f4461b552758
yxhej <wui@bxscience.edu>
2024-08-20 23:49:10 -0400

    attempt display & add button logic

--- sketch.ino
+++ sketch.ino
@@ -1,4 +1,6 @@
+#include <SPI.h>
 #include "DHT.h"
+#include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 
@@ -9,9 +11,10 @@
 
 #define SCREEN_WIDTH 128 // OLED display width, in pixels
 #define SCREEN_HEIGHT 64 // OLED display height, in pixels
+#define buttonPin 2
 
 // Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
-// Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, , -1);
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
 
 
 // DHT22 declaration
@@ -25,13 +28,24 @@ void setup() {
   Serial1.begin(115200);
   Serial1.println("Hello, Raspberry Pi Pico W!");
 
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(0, 10);
+  display.println("test");
+
+  pinMode(buttonPin, INPUT_PULLUP);
   dht.begin();
   update();
 }
 
 void loop() {
-  delay(2000);
+  int measurement = digitalRead(buttonPin);
 
+  if (measurement == 0) {
+    update();
+    Serial1.println("win");
+  }
 }
 
 void playSound(float temp, double seconds) {
